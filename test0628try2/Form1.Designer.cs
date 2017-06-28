cc5081fec5303b3123fa12cd00e370c2f1f5bc4b
hsinfeng <judy4a031004@gmail.com>
2017-06-28 16:34:31 +0800

    aaa

--- test0628try2/Form1.Designer.cs
+++ test0628try2/Form1.Designer.cs
@@ -28,7 +28,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(56, 32);
+            this.button1.Location = new System.Drawing.Point(55, 52);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(195, 58);
             this.button1.TabIndex = 0;
